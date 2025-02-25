0d006202a7f8e34aa16e8d6fa30d2c4b99a8060a
Kevin Fowler <kevin.m.fowler17@gmail.com>
2025-02-25 15:34:05 -0600

    first commit

---
+++ src/test/java/com/mythologyproject/mythology_project/MythologyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mythologyproject.mythology_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythologyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
